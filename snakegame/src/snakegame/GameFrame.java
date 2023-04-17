a7d80b83d91fe013e1c8e354b6b72018f0c3f54d
k1m2njun <k1m2njun@naver.com>
2023-04-17 19:23:38 +0900

    Complete

---
+++ snakegame/src/snakegame/GameFrame.java
@@ -0,0 +1,20 @@
+package snakegame;
+
+import javax.swing.JFrame;
+
+public class GameFrame extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+
+	GameFrame() {
+		GamePanel panel = new GamePanel();
+		this.add(panel);
+		this.setTitle("뱀");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		this.pack();
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);
+	}
+
+}
---
+++ snakegame/src/snakegame/SnakeGame.java
@@ -0,0 +1,14 @@
+package snakegame;
+
+public class SnakeGame {
+	
+	public static void main(String[] args) {
+		new GameFrame();
+	}
+}
+
+/*
+	https://www.youtube.com/watch?v=S4lPjokjHWs
+	Build a Snake Game in Java - QUICK and EASY TUTORIAL
+	Devression
+*/
